44bade81e9a2469fa7476d9c88f661aa3dbaaca5
Diego <diegoccpinto@gmail.com>
2023-02-03 11:48:59 -0300

    Created from functionality in JavaScript

---
+++ index.js
@@ -0,0 +1,53 @@
+const newAnnotation = document.getElementById("new_annotation");
+const createdButton = document.getElementById("button_create");
+const taskList = document.getElementById("task-list");
+const tarefasCriadas = document.getElementById("number_of_tasks");
+const tasksRegister = document.getElementById("tasks_register");
+
+
+let listaDeTarefas = [];
+
+
+
+if (listaDeTarefas.length == 0) {
+    let listaVazia = document.createElement("div");
+    listaVazia.className = "list-empty-container"
+
+    tasksRegister.append(listaVazia);
+    listaVazia.innerHTML += `
+        <img id="img_clipboard" src="image/Clipboard.png"/>
+        <div class="register_empty">
+            <p id="not_registered">Você ainda não tem tarefas cadastradas</p>
+            <p>Crie tarefas e organize seus itens a fazer</p>
+        </div>
+    `
+}
+
+
+function tarefasAdicionadas() {
+    // adiciona uma tarefa na lista
+    taskList.innerHTML = '';
+    for (let tarefa in listaDeTarefas) {
+        taskList.innerHTML += `                
+        <li class="item">${listaDeTarefas[tarefa]}</li>
+        `
+    }
+}
+
+
+createdButton.addEventListener("click", () => {
+
+    if (newAnnotation.value == "") {
+        return;
+    }
+    listaDeTarefas.push(newAnnotation.value);
+    newAnnotation.value = ""
+    tarefasAdicionadas();
+    // conta a quantidade na lista
+    tarefasCriadas.textContent = listaDeTarefas.length;
+
+    document.querySelector(".list-empty-container").remove();
+
+
+});
+

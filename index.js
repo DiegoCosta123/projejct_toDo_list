48c66202db7b8a711c5376cf1f27ce5afd362bc0
Diego <diegoccpinto@gmail.com>
2023-02-06 12:50:31 -0300

    Ajuste da lista for identificador e valor, clicar icone para remover

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const createdButton = document.getElementById("button_create");
 const taskList = document.getElementById("task-list");
 const tarefasCriadas = document.getElementById("number_of_tasks");
 const tasksRegister = document.getElementById("tasks_register");
+const iconRemove = document.getElementById("icon_trash");
 
 
 let listaDeTarefas = [];
@@ -23,16 +24,23 @@ if (listaDeTarefas.length == 0) {
     `
 }
 
+function generateID() {
+    const id = Math.random().toString(16).slice(2);
+    return id;
+}
 
 function tarefasAdicionadas() {
     // adiciona uma tarefa na lista
     taskList.innerHTML = '';
     for (let tarefa in listaDeTarefas) {
         taskList.innerHTML += `
-            <div class="Teste">
-                <div id="teste">
-                    <input type="checkbox" name="checkConcluded" id="check_concluded">
-                    <li class="item">${listaDeTarefas[tarefa]}</li>
+            <div class="annotation_field_container">
+                <div id="annotation_field">
+                    <input onclick="tarefaFeita()" type="checkbox" name="checkConcluded" id="check_concluded">
+                    <div id="item_container">
+                        <li class="item">${listaDeTarefas[tarefa].value}</li>
+                    </div>
+                    <img onclick="removerAnnotation('${listaDeTarefas[tarefa].id}')" id="icon_trash" src="image/trash.png"/>
                 </div>
             </div>
         `
@@ -40,19 +48,58 @@ function tarefasAdicionadas() {
 }
 
 
-createdButton.addEventListener("click", () => {
+function atualizarListaDeTarefas() {
+    tarefasCriadas.textContent = listaDeTarefas.length;
+}
 
+// Remover da Lista
+function removerAnnotation(idQueFoiClicado) {
+    const idsDaListaDeTarefas = listaDeTarefas;
+
+    for (let i = 0; i < idsDaListaDeTarefas.length; i++) {
+        if (idsDaListaDeTarefas[i].id == idQueFoiClicado) {
+            idsDaListaDeTarefas.splice(i, 1);
+            break;
+        }
+    }
+    // conta a quantidade na lista
+    atualizarListaDeTarefas();
+    tarefasAdicionadas();
+
+}
+
+function adicionarTarefa() {
     if (newAnnotation.value == "") {
         return;
     }
-    listaDeTarefas.push(newAnnotation.value);
+
+    const data = {
+        id: generateID(),
+        value: newAnnotation.value,
+        isDone: false
+    }
+
+    listaDeTarefas.push(data);
     newAnnotation.value = ""
     tarefasAdicionadas();
     // conta a quantidade na lista
-    tarefasCriadas.textContent = listaDeTarefas.length;
+    atualizarListaDeTarefas();
 
     document.querySelector(".list-empty-container").remove();
+}
 
+// Lista for feita e clicar checkout
+function tarefaFeita() {
 
-});
+    const idsDaListaDeTarefas = listaDeTarefas;
+    for (let i = 0; i < idsDaListaDeTarefas.length; i++) {
+        if (idsDaListaDeTarefas[i].id == idQueFoiClicado) {
+            idsDaListaDeTarefas.splice(i, 1);
+            break;
+        }
+    }
+    // conta a quantidade na lista
+    atualizarListaDeTarefas();
+    tarefasAdicionadas();
+}
 

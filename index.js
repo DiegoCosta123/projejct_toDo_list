9cfd269302dfe21b8fa61200fc74ef46d43f07f6
Diego <diegoccpinto@gmail.com>
2023-02-07 13:41:58 -0300

    Ajustado checkot vetor

--- index.js
+++ index.js
@@ -2,33 +2,40 @@ const newAnnotation = document.getElementById("new_annotation");
 const createdButton = document.getElementById("button_create");
 const taskList = document.getElementById("task-list");
 const tarefasCriadas = document.getElementById("number_of_tasks");
+const tarefasFeitas = document.getElementById("number_of_tasks_completed");
+
 const tasksRegister = document.getElementById("tasks_register");
 const iconRemove = document.getElementById("icon_trash");
 const arrayList = document.getElementsByClassName("annotation_field_container");
+const isdone = document.getElementById("check_concluded");
 
 
 let listaDeTarefas = [];
 
 
+function criarListaVazia() {
+    if (listaDeTarefas.length == 0) {
+        let listaVazia = document.createElement("div");
+        listaVazia.className = "list-empty-container"
 
-if (listaDeTarefas.length == 0) {
-    let listaVazia = document.createElement("div");
-    listaVazia.className = "list-empty-container"
+        tasksRegister.append(listaVazia);
+        listaVazia.innerHTML += `
+            <img id="img_clipboard" src="image/Clipboard.png"/>
+            <div class="register_empty">
+                <p id="not_registered">Você ainda não tem tarefas cadastradas</p>
+                <p>Crie tarefas e organize seus itens a fazer</p>
+            </div>
+        `
+    }
 
-    tasksRegister.append(listaVazia);
-    listaVazia.innerHTML += `
-        <img id="img_clipboard" src="image/Clipboard.png"/>
-        <div class="register_empty">
-            <p id="not_registered">Você ainda não tem tarefas cadastradas</p>
-            <p>Crie tarefas e organize seus itens a fazer</p>
-        </div>
-    `
 }
+
+criarListaVazia();
 
 function generateID() {
     const id = Math.random().toString(16).slice(2);
     return id;
-}
+} //vc tinha colocado ,muitos ID, ele substituiu por classe
 
 function tarefasAdicionadas() {
     // adiciona uma tarefa na lista
@@ -36,21 +43,37 @@ function tarefasAdicionadas() {
     for (let tarefa in listaDeTarefas) {
         taskList.innerHTML += `
             <div class="annotation_field_container">
-                <div id="annotation_field">
-                    <input onclick="tarefaFeita()" type="checkbox" name="checkConcluded" id="check_concluded">
-                    <div id="item_container">
-                        <li class="item">${listaDeTarefas[tarefa].value}</li>
+                <div class="id${listaDeTarefas[tarefa].id} annotation_field">
+                    <div class="check_task_icon_container">
+                        <input onchange="tarefaFeita('${listaDeTarefas[tarefa].id}')" type="checkbox" name="checkConcluded" class="check_concluded"/>
+                        <img class="check_vetor" src="image/vector.png"/>                    
                     </div>
-                    <img onclick="removerAnnotation('${listaDeTarefas[tarefa].id}')" id="icon_trash" src="image/trash.png"/>
+                    <div class="item_container">
+                        <p class="item">${listaDeTarefas[tarefa].value}</p>
+                    </div>
+                    <img onclick="removerAnnotation('${listaDeTarefas[tarefa].id}')" class="icon_trash" src="image/trash.png"/>
                 </div>
             </div>
         `
+        // <img id="checkVetor" src="image/vector.png"/>
     }
 }
 
-
+// atualiza o numero de tarefas criadas
 function atualizarListaDeTarefas() {
     tarefasCriadas.textContent = listaDeTarefas.length;
+}
+
+// atualiza o numero de tarefas completadas
+function atualizarListaDeTarefasFeitas() {
+    let isChecked = 0;
+
+    for (let i = 0; i < listaDeTarefas.length; i++) {
+        if (listaDeTarefas[i].isDone) {
+            isChecked++;
+        }
+    }
+    tarefasFeitas.textContent = isChecked;
 }
 
 // Remover da Lista
@@ -66,6 +89,8 @@ function removerAnnotation(idQueFoiClicado) {
     // conta a quantidade na lista
     atualizarListaDeTarefas();
     tarefasAdicionadas();
+    criarListaVazia();
+    atualizarListaDeTarefasFeitas();
 
 }
 
@@ -85,20 +110,30 @@ function adicionarTarefa() {
     tarefasAdicionadas();
     // conta a quantidade na lista
     atualizarListaDeTarefas();
-
-    document.querySelector(".list-empty-container").remove();
+    if (listaDeTarefas.length == 1) {
+        document.querySelector(".list-empty-container").remove();
+    }
 }
 
 // Lista for feita e clicar checkout
-// function tarefaFeita() {
-//     for (let i = 0; i < listaDeTarefas.length; i++) {
-//         if ([i].id == idQueFoiClicado) {
-//             idsDaListaDeTarefas.(i, 1);
-//             break;
-//         }
-//     }
-//     // conta a quantidade na lista
-//     atualizarListaDeTarefas();
-//     tarefasAdicionadas();
-// }
+function tarefaFeita(idQueFoiClicado) {
+    const idsDaListaDeTarefas = listaDeTarefas;
+    let isDone = document.querySelector(`.id${idQueFoiClicado} .check_concluded`);
+    const checked = document.querySelector(`.id${idQueFoiClicado} .item`);
+
+    for (let i = 0; i < idsDaListaDeTarefas.length; i++) {
+
+        if (idsDaListaDeTarefas[i].id == idQueFoiClicado) {
+            if (isDone.checked) {
+                checked.classList.add("checked");
+                idsDaListaDeTarefas[i].isDone = true;
+            } else {
+                checked.classList.remove("checked");
+
+                idsDaListaDeTarefas[i].isDone = false;
+            }
+        }
+    }
+    atualizarListaDeTarefasFeitas();
+}
 
